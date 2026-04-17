eb6cc2866d2d1b9441eb5a2ecdd2c17cf29ab98c
Sandy Maguire <sandy.maguire@tweag.io>
2026-04-17 13:39:35 -0700

    Fix Vouch

--- src/Distribution/Server/Features/Vouch.hs
+++ src/Distribution/Server/Features/Vouch.hs
@@ -1,9 +1,13 @@
-{-# LANGUAGE NamedFieldPuns #-}
-{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE MonoLocalBinds        #-}
+{-# LANGUAGE NamedFieldPuns        #-}
+{-# LANGUAGE RankNTypes            #-}
 
 module Distribution.Server.Features.Vouch (VouchFeature(..), initVouchFeature, judgeVouch) where
 
-import qualified Distribution.Server.Features.Vouch.State as Acid
+import Distribution.Verbosity (normal)
+import Distribution.Server.Database.Schemas.Features (VouchRow(..), vouchesSchema)
 import Distribution.Server.Features.Vouch.Types
 import Control.Monad (when, join)
 import Control.Monad.Except (runExceptT, throwError)
@@ -14,43 +18,57 @@ import Data.Time (UTCTime(..), addUTCTime, getCurrentTime, nominalDay, secondsTo
 import Data.Time.Format.ISO8601 (formatShow, iso8601Format)
 import Text.XHtml.Strict (prettyHtmlFragment, stringToHtml, li)
 
-import Distribution.Server.Framework ((</>), AcidState, DynamicPath, HackageFeature, IsHackageFeature, IsHackageFeature(..))
-import Distribution.Server.Framework (MessageSpan(MText), Method(..), Response, ServerEnv(..), ServerPartE, StateComponent(..))
-import Distribution.Server.Framework (abstractAcidStateComponent, emptyHackageFeature, errBadRequest)
+import Distribution.Server.Framework ((</>), DynamicPath, HackageFeature, IsHackageFeature, IsHackageFeature(..), internalServerErrorResponse, lognotice)
+import Distribution.Server.Framework (MessageSpan(MText), Method(..), Response, ServerEnv(..), ServerPartE)
+import Distribution.Server.Framework (emptyHackageFeature, errBadRequest)
 import Distribution.Server.Framework (featureDesc, featureReloadFiles, featureResources, featureState)
-import Distribution.Server.Framework (liftIO, openLocalStateFrom, query, queryState, resourceAt, resourceDesc, resourceGet)
-import Distribution.Server.Framework (resourcePost, toResponse, update, updateState)
-import Distribution.Server.Framework.BackupRestore (RestoreBackup(..))
+import Distribution.Server.Framework (liftIO, resourceAt, resourceDesc, resourceGet)
+import Distribution.Server.Framework (resourcePost, toResponse)
 import Distribution.Server.Framework.Templating (($=), TemplateAttr, getTemplate, loadTemplates, reloadTemplates, templateUnescaped)
 import qualified Distribution.Server.Users.Group as Group
 import Distribution.Server.Users.Types (UserId(..), UserInfo, UserName(..), userName)
 import Distribution.Server.Features.Upload(UploadFeature(..))
 import Distribution.Server.Features.Users (UserFeature(..))
 import Distribution.Simple.Utils (toUTF8LBS)
+import Rel8 hiding (null, run)
+import qualified Rel8 as Rel8
+import           Hasql.Connection (Connection)
+import           Hasql.Session (SessionError, statement, run)
 
-vouchStateComponent :: FilePath -> IO (StateComponent AcidState Acid.VouchData)
-vouchStateComponent stateDir = do
-  st <- openLocalStateFrom (stateDir </> "db" </> "Vouch") (Acid.VouchData mempty mempty)
-  let initialVouchData = Acid.VouchData mempty mempty
-      restore =
-        RestoreBackup
-          { restoreEntry = error "Unexpected backup entry"
-          , restoreFinalize = return initialVouchData
-          }
-  pure StateComponent
-    { stateDesc = "Keeps track of vouches"
-    , stateHandle = st
-    , getState = query st Acid.GetVouchesData
-    , putState = update st . Acid.ReplaceVouchesData
-    , backupState = \_ _ -> []
-    , restoreState = restore
-    , resetState = vouchStateComponent
-    }
+doSelect
+    :: Serializable exprs (FromExprs exprs)
+    => Connection
+    -> Query exprs
+    -> IO (Either SessionError [FromExprs exprs])
+doSelect conn = flip run conn . statement () . Rel8.run . select
+
+
+doSelectE
+    :: Serializable exprs (FromExprs exprs)
+    => Connection
+    -> Query exprs
+    -> ServerPartE [FromExprs exprs]
+doSelectE conn q = do
+  liftIO (doSelect conn q) >>= \case
+    Left err -> do
+      lognotice normal $ show err
+      throwError internalServerErrorResponse
+    Right a -> pure a
+
+doUpdate
+    :: Serializable exprs (FromExprs exprs)
+    => Connection
+    -> Update (Query exprs)
+    -> IO (Either SessionError [FromExprs exprs])
+doUpdate conn = flip run conn . statement () . Rel8.run . update
+
+doInsert :: Connection -> Insert a -> IO (Either SessionError ())
+doInsert conn = flip run conn . statement () . Rel8.run_ . insert
 
 data VouchFeature =
   VouchFeature
     { vouchFeatureInterface :: HackageFeature
-    , drainQueuedNotifications :: forall m. MonadIO m => m [UserId]
+    , drainQueuedNotifications :: forall m. (MonadIO m, MonadFail m) => m [UserId]
     }
 
 instance IsHackageFeature VouchFeature where
@@ -111,8 +129,7 @@ renderVouchers lookupUserInfo (uid, timestamp) = do
   pure . toUTF8LBS . prettyHtmlFragment . li . stringToHtml $ name <> " vouched on " <> formatShow iso8601Format newUTCTime
 
 initVouchFeature :: ServerEnv -> IO (UserFeature -> UploadFeature -> IO VouchFeature)
-initVouchFeature ServerEnv{serverStateDir, serverTemplatesDir, serverTemplatesMode} = do
-  vouchState <- vouchStateComponent serverStateDir
+initVouchFeature ServerEnv{serverConnection = conn, serverTemplatesDir, serverTemplatesMode} = do
   templates <- loadTemplates serverTemplatesMode [ serverTemplatesDir, serverTemplatesDir </> "Html"]
                                                  ["vouch.html"]
   vouchTemplate <- getTemplate templates "vouch.html"
@@ -122,7 +139,7 @@ initVouchFeature ServerEnv{serverStateDir, serverTemplatesDir, serverTemplatesMo
       handleGetVouches :: DynamicPath -> ServerPartE Response
       handleGetVouches dpath = do
         uid <- lookupUserName =<< userNameInPath dpath
-        vouches <- queryState vouchState $ Acid.GetVouchesFor uid
+        vouches <- doSelectE conn $ getVouchesFor uid
         param <- renderToLBS lookupUserInfo vouches
         pure . toResponse $ vouchTemplate
           [ "msg" $= ""
@@ -135,8 +152,8 @@ initVouchFeature ServerEnv{serverStateDir, serverTemplatesDir, serverTemplatesMo
         ugroup <- liftIO $ Group.queryUserGroup uploadersGroup
         now <- liftIO getCurrentTime
         vouchee <- lookupUserName =<< userNameInPath dpath
-        vouchersForVoucher <- queryState vouchState $ Acid.GetVouchesFor voucher
-        existingVouchers <- queryState vouchState $ Acid.GetVouchesFor vouchee
+        vouchersForVoucher <- doSelectE conn $ getVouchesFor voucher
+        existingVouchers <- doSelectE conn $ getVouchesFor vouchee
         case judgeVouch ugroup now vouchee vouchersForVoucher existingVouchers voucher of
           Left NotAnUploader ->
             errBadRequest "Not an uploader" [MText "You must be an uploader yourself to endorse other users."]
@@ -149,17 +166,11 @@ initVouchFeature ServerEnv{serverStateDir, serverTemplatesDir, serverTemplatesMo
           Left YouAlreadyVouched ->
             errBadRequest "Already endorsed" [MText "You have already endorsed this user."]
           Right result -> do
-            updateState vouchState $ Acid.PutVouch vouchee (voucher, now)
+            liftIO $ doInsert conn $
+              putVouch vouchee voucher now
             param <- renderToLBS lookupUserInfo $ existingVouchers ++ [(voucher, now)]
             case result of
               AddVouchComplete -> do
-                -- enqueue vouching completed notification
-                -- which will be read using drainQueuedNotifications
-                Acid.VouchData vouches notNotified <-
-                  queryState vouchState Acid.GetVouchesData
-                let newState = Acid.VouchData vouches (Set.insert vouchee notNotified)
-                updateState vouchState $ Acid.ReplaceVouchesData newState
-
                 liftIO $ Group.addUserToGroup uploadersGroup vouchee
                 pure . toResponse $ vouchTemplate
                   [ "msg" $= "Added endorsement. User is now an uploader!"
@@ -188,13 +199,61 @@ initVouchFeature ServerEnv{serverStateDir, serverTemplatesDir, serverTemplatesMo
               , resourcePost = [("html", handlePostVouch)]
               }
             ]
-          , featureState = [ abstractAcidStateComponent vouchState ]
+          , featureState = []
           , featureReloadFiles = reloadTemplates templates
           },
       drainQueuedNotifications = do
-        Acid.VouchData vouches notNotified <-
-          queryState vouchState Acid.GetVouchesData
-        let newState = Acid.VouchData vouches mempty
-        updateState vouchState $ Acid.ReplaceVouchesData newState
-        pure $ Set.toList notNotified
+        Right notNotified <- liftIO $ doUpdate conn getAndClearUnnotifiedVouchees
+        pure $ Set.toList $ Set.fromList notNotified
+    }
+
+
+-- | Insert a new vouch
+putVouch
+  :: UserId
+  -- ^ Vouchee
+  -> UserId
+  -- ^ Voucher
+  -> UTCTime
+  -> Insert ()
+putVouch vouchee voucher now =
+  Insert
+  { into = vouchesSchema
+  , rows = values
+      [ VouchRow
+          { vId = unsafeDefault
+          , vVouchee = lit vouchee
+          , vVoucher = lit voucher
+          , vTime = lit now
+          , vNotified =
+              -- enqueue vouching completed notification
+              -- which will be read using drainQueuedNotifications
+              lit False
+          }
+      ]
+  , onConflict = Abort
+  , returning = NoReturning
+  }
+
+
+-- | Return all vouchers (and times) for a given vouchee.
+getVouchesFor :: UserId -> Query (Expr UserId, Expr UTCTime)
+getVouchesFor uid = do
+  vouch <- each vouchesSchema
+  where_ $ vVouchee vouch ==. lit uid
+  pure (vVoucher vouch, vTime vouch)
+
+
+-- | Get all vouchees for whom 'vNotified' is 'False', and atomically set
+-- 'vNotified' to 'True'.
+getAndClearUnnotifiedVouchees :: Update (Query (Expr UserId))
+getAndClearUnnotifiedVouchees = do
+  Update
+    { target = vouchesSchema
+    , from = pure ()
+    , set = const $ \vouch ->
+        vouch { vNotified = lit True }
+    , updateWhere = const $ \vouch -> do
+        vNotified vouch ==. lit False
+    , returning = Returning vVouchee
     }
