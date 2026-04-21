73db805cccc9c743d61274fc872d46bf8e981a92
Sandy Maguire <sandy@sandymaguire.me>
2026-04-21 11:07:32 -0700

    DB helpers
    more helpers

--- src/Distribution/Server/Framework/DB.hs
+++ src/Distribution/Server/Framework/DB.hs
@@ -60,18 +60,28 @@ doSelect1
     -> IO (Either SessionError (FromExprs exprs))
 doSelect1 conn = flip run conn . statement () . Rel8.run1 . select
 
+toE :: IO (Either SessionError a) -> ServerPartE a
+toE ma = do
+  liftIO ma >>= \case
+    Left err -> do
+      lognotice normal $ show err
+      throwError internalServerErrorResponse
+    Right a -> pure a
 
 doSelectE
     :: Serializable exprs (FromExprs exprs)
     => Connection
     -> Query exprs
     -> ServerPartE [FromExprs exprs]
-doSelectE conn q = do
-  liftIO (doSelect conn q) >>= \case
-    Left err -> do
-      lognotice normal $ show err
-      throwError internalServerErrorResponse
-    Right a -> pure a
+doSelectE conn = toE . doSelect conn
+
+
+doSelect1E
+    :: Serializable exprs (FromExprs exprs)
+    => Connection
+    -> Query exprs
+    -> ServerPartE (FromExprs exprs)
+doSelect1E conn = toE . doSelect1 conn
 
 doUpdate
     :: Serializable exprs (FromExprs exprs)
@@ -88,5 +98,70 @@ doUpdate1
 doUpdate1 conn = flip run conn . statement () . Rel8.run1 . update
 
 
-doInsert :: Connection -> Insert a -> IO (Either SessionError ())
-doInsert conn = flip run conn . statement () . Rel8.run_ . insert
+doUpdateE
+    :: Serializable exprs (FromExprs exprs)
+    => Connection
+    -> Update (Query exprs)
+    -> ServerPartE [FromExprs exprs]
+doUpdateE conn = toE . doUpdate conn
+
+
+doInsert
+    :: Serializable exprs (FromExprs exprs)
+    => Connection
+    -> Insert (Query exprs)
+    -> IO (Either SessionError [FromExprs exprs])
+doInsert conn = flip run conn . statement () . Rel8.run . insert
+
+
+doInsert1
+    :: Serializable exprs (FromExprs exprs)
+    => Connection
+    -> Insert (Query exprs)
+    -> IO (Either SessionError (FromExprs exprs))
+doInsert1 conn = flip run conn . statement () . Rel8.run1 . insert
+
+
+doInsertE
+    :: Serializable exprs (FromExprs exprs)
+    => Connection
+    -> Insert (Query exprs)
+    -> ServerPartE [FromExprs exprs]
+doInsertE conn = toE . doInsert conn
+
+
+doInsert_
+    :: Connection
+    -> Insert a
+    -> IO (Either SessionError ())
+doInsert_ conn = flip run conn . statement () . Rel8.run_ . insert
+
+
+doInsertE_
+    :: Connection
+    -> Insert a
+    -> ServerPartE ()
+doInsertE_ conn = toE . doInsert_ conn
+
+
+doDelete
+    :: Serializable exprs (FromExprs exprs)
+    => Connection
+    -> Delete (Query exprs)
+    -> IO (Either SessionError [FromExprs exprs])
+doDelete conn = flip run conn . statement () . Rel8.run . delete
+
+
+doDeleteE
+    :: Serializable exprs (FromExprs exprs)
+    => Connection
+    -> Delete (Query exprs)
+    -> ServerPartE [FromExprs exprs]
+doDeleteE conn = toE . doDelete conn
+
+
+doDelete_ :: Connection -> Delete a -> IO (Either SessionError ())
+doDelete_ conn = flip run conn . statement () . Rel8.run_ . delete
+
+doDeleteE_ :: Connection -> Delete a -> ServerPartE ()
+doDeleteE_ conn = toE . doDelete_ conn
